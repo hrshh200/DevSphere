fa0dc85b6428eac681a42e836f6bd683b018b465
Anushka <anushkakohli2021@gmail.com>
2024-08-19 20:51:29 +0530

    hello anushka

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,14 @@
 "use client";
 
-import {styles} from "./Home.module.css"
+import { styles } from "./Home.module.css";
 
-const Home = () =>{
-    return(
-        <div>
-            <h1>Hello harsh</h1>
-        </div>
-    );
-}
+const Home = () => {
+  return (
+    <div>
+      <h1>Hello harsh</h1>
+      <h1>Hello anushka</h1>
+    </div>
+  );
+};
 
 export default Home;
