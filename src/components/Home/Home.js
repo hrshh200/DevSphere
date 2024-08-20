c4c6fea705b6462fade67f1cd30e033d3c9d9fb1
Harsh Sharma <114438275+hrshh200@users.noreply.github.com>
2024-08-20 15:53:18 +0530

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,51 @@
 "use client";
 
-import { styles } from "./Home.module.css";
+import styles from "./Home.module.css";
+import Dashboard from "../Dashboard/Dashboard";
 
 const Home = () => {
-  return (
-    <div>
-      <h1>Hello harsh</h1>
-      <h1>Hello anushka</h1>
-    </div>
-  );
+    return (
+        <div>
+            <Dashboard />
+            <div className={styles.homeStyle}>
+                <section className={styles.mainText}>
+                    <div className={styles.mainTextContent}>
+                        <div className={styles.labelStyle}>
+                            <div className={styles.innerlabelStyle}>
+                                <h2>
+                                    CREATING
+                                </h2>
+                                <h1>
+                                    custom
+                                </h1>
+                            </div>
+                            <div className={styles.innerlabelStyle}>
+                                <h1>
+                                    digital
+                                </h1>
+                                <h2>
+                                    SOLUTIONS
+                                </h2>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <div className={styles.innerPara}>
+                    <p>
+                    We are a cooperative of like minded professionals skilled at solving problems and creating excellent products for today's digital world.
+                    </p>
+                </div>
+                <div className={styles.buttonDisplay}>
+                    <div className={styles.talkButton}>
+                        <button>Let's talk!</button>
+                    </div>
+                    <div className={styles.serviceButton}>
+                        <button>Services</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Home;
